846e82f3ea289ed47875d4a5d66f75ae30b54427
Martín Mtz <martin.ml@icloud.com>
2018-11-23 14:22:39 -0600

    v0

--- public/app/views/main/main.controller.js
+++ public/app/views/main/main.controller.js
@@ -9,20 +9,10 @@
 
   function MainController($state, $scope, $http){
 
-    var _this = this;
-
-    $scope.colors = ['#4CA90C','#4CA90C','#71C5E8','#71C5E8','#FFB81C','#FFB81C','#009FDB'];
-
-
     $scope.header = ['Canal','Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct'];
-    $http.get("../../../1.json").success(function (data) { $scope.canalaccesos = data; }).error(function (data) {console.log("there was an error"); });
-    $http.get("../../../2.json").success(function (data) { $scope.canalPagoFactura = data; }).error(function (data) {console.log("there was an error"); });
-    $http.get("../../../3.json").success(function (data) { $scope.canalRecargas = data; console.log(data); }).error(function (data) {console.log("there was an error"); });
-    $http.get("../../../4.json").success(function (data) { $scope.plataformaaccesos = data; }).error(function (data) {console.log("there was an error"); });
-    $http.get("../../../5.json").success(function (data) { $scope.plataformaPagoFactura = data; }).error(function (data) {console.log("there was an error"); });
-    $http.get("../../../6.json").success(function (data) { $scope.plataformaRecargas = data; }).error(function (data) {console.log("there was an error"); });
-
-
+    $http.get("../../../01.json").success(function (data) { $scope.accesos = data; }).error(function (data) {console.log("there was an error"); });
+    $http.get("../../../02.json").success(function (data) { $scope.pagoFactura = data; }).error(function (data) {console.log("there was an error"); });
+    $http.get("../../../03.json").success(function (data) { $scope.recargas = data; }).error(function (data) {console.log("there was an error"); });
 
   $scope.isString = function(val){
       return isNaN(val);
@@ -36,13 +26,15 @@
   $scope.labelChart = '';
   $scope.lastWrapper = '';
   $scope.lastChart = '';
-  $scope.full = function(obj, label, id, lastWrapper){
+
+  $scope.full = function(obj, label, id, lastWrapper, icon){
     var element = document.getElementById(id);
     var xd = document.getElementById('xd');
     var wrapper = document.getElementById('wrapper');
-    wrapper.style.top = (element.offsetTop - 70) + 'px';
+    wrapper.style.top = (element.offsetTop - 88) + 'px';
     setTimeout(function(){
       $scope.labelChart = label;
+      $scope.icon = icon;
       $scope.lastWrapper = lastWrapper;
       $scope.lastChart = id;
       $scope.fullScreen = true;
@@ -53,7 +45,6 @@
   };
   $scope.min = function(){
     $scope.fullScreen = false;
-    // $scope.chartfill.destroy();
     var element = document.getElementById($scope.lastChart);
 
     setTimeout(function(){
@@ -62,83 +53,33 @@
     },300);
   };
 
-    $scope.pageIndex = 0;
-    this.mainOptions = {
-      sectionsColor: ['rgb(255,255,255)', 'rgb(255,255,255)', 'rgb(255,255,255)'],
-      navigation: true,
-      scrollingSpeed: 1000,
-      change: (page, next) => {
-       $scope.pageIndex = next - 1;
-       $scope.preFactory();
-      }
-    };
-
-    $scope.parseChart = function(obj){
-      var item = {
-        fill: false,
-        label: "IVR Visual",
-        backgroundColor: '#CAA2DD',
-        pointRadius: 4,
-        pointBorderWidth: 2,
-        borderWidth: 1,
-        borderColor: '#CAA2DD',
-        data: [],
-        hidden: true
-      };
-      
-      obj.chart = [];
-      for(var i in obj.table){
-          obj.chart[i] = angular.copy(item);
-          obj.chart[i].label = obj.table[i][0];
-          obj.chart[i].backgroundColor = $scope.colors[i];
-          obj.chart[i].borderColor = $scope.colors[i];
-          obj.chart[i].data = obj.table[i].slice(1);
-          console.log(i);
-          if(obj.table.length === (Number(i)+1)){
-            obj.chart[i].hidden = false;
-            obj.chart[i].borderWidth = 2;
-          }
-      }
-    };
-
-    $scope.preFactory = function(){
-      var chart = [];
-      var newChart = [];
-      switch($scope.pageIndex){
-        case 0: chart = $scope.accesos.chart; break;
-        case 1: chart = $scope.pagoFactura.chart; break;
-        case 2: chart = $scope.recargas.chart; break;
-      };
-
-      if($scope.chartDisplayAll){
-        newChart = chart;
-      }else{
-        newChart[0] = chart[chart.length - 1];
-      }
-      $scope.factoryLine(newChart, 'myChart1');
-      // $scope.factoryBar(newChart, 'myChart1');
-
-    };
-   
-
     $scope.factoryLine = function(obj, id){
-
 
       var ctx = document.getElementById(id).getContext('2d');
           Chart.defaults.global.defaultFontSize = 10;
-          // $scope.chart && $scope.chart.destroy();
           var myChart = new Chart(ctx, {
           type: 'line',
           data: {
-              labels: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct'],
-              datasets: obj
+              labels: obj.labels,
+              datasets: [
+                {
+                  fill: false,
+                  label: "IVR Visual",
+                  backgroundColor: '#009FDB',
+                  pointRadius: 4,
+                  pointBorderWidth: 2,
+                  borderWidth: 1,
+                  borderColor: '#009FDB',
+                  data: obj.data,
+                  hidden: false
+                }
+              ]
           },
           options: {
               responsive: true, 
               maintainAspectRatio: false,
               legend: {
-                  // onClick: (e) => e.stopPropagation(),
-                  display: true,
+                  display: false,
                   labels: {
                       boxWidth: 10,
                       boxHeight: 20
@@ -158,40 +99,6 @@
       return myChart;
   };
 
-  $scope.factoryBar = function(obj, id){
-    var ctx = document.getElementById(id).getContext('2d');
-        Chart.defaults.global.defaultFontSize = 8;
-        $scope.chart && $scope.chart.destroy();
-        $scope.chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
-
-        // The data for our dataset
-        data: {
-            labels: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct'],
-            datasets: obj
-        },
-
-        // Configuration options go here
-        options: {
-            responsive: true, 
-            maintainAspectRatio: false, 
-            legend: {
-                display: true
-            },
-            scales: {
-              xAxes: [{
-                stacked: true,
-              }],
-              yAxes: [{
-                stacked: true
-              }]
-            }
-
-        }
-    });
-};
-
     $scope.tabsToggle = function(tab){
       for(var i in $scope.tabs){
         $scope.tabs[i].active = false;
@@ -199,42 +106,30 @@
       tab.active = true;
     };
 
+
+    $scope.parseToChart = function(data){
+      var obj = { labels:[], data: [] };
+
+      for(var i in data){
+        var item = data[i];
+        var monto = item.monto || item.cantidad;
+        monto = monto.replace("$","");
+        monto = monto.replace(",","");
+        obj.labels.push(item.periodo);
+        obj.data.push(monto);
+      }
+      return obj;
+    };
+
     $scope.init = function(){
 
-      $scope.accesos = { 
-        table: $scope.canalaccesos,
-          chart:[] 
-      };
-  
-      $scope.pagoFactura = { 
-        table: $scope.canalPagoFactura,
-          chart:[] 
-      };
-      $scope.recargas = { 
-        table: $scope.canalRecargas,
-          chart:[] 
-      };
+      $scope.accesosChart = $scope.parseToChart($scope.accesos);
+      $scope.pagoFacturaChart = $scope.parseToChart($scope.pagoFactura);
+      $scope.recargasChart = $scope.parseToChart($scope.recargas);
 
-      $scope.tabs = [
-        { title: "Datos por Canal", active: true, jsons: {
-          accesos: $scope.canalaccesos,
-          pagoFactura: $scope.canalPagoFactura,
-          recargas: $scope.canalRecargas
-        } },
-        { title: "Graficas por Canal", active: false, jsons: {
-          accesos: $scope.plataformaaccesos,
-          pagoFactura: $scope.plataformaPagoFactura,
-          recargas: $scope.plataformaRecargas
-        }}
-      ];
-
-      $scope.parseChart($scope.accesos);
-      $scope.parseChart($scope.pagoFactura);
-      $scope.parseChart($scope.recargas);
-
-      $scope.factoryLine($scope.accesos.chart, 'myChart1');
-      $scope.factoryLine($scope.pagoFactura.chart, 'myChart2');
-      $scope.factoryLine($scope.recargas.chart, 'myChart3');
+      $scope.factoryLine($scope.accesosChart, 'myChart1');
+      $scope.factoryLine($scope.pagoFacturaChart, 'myChart2');
+      $scope.factoryLine($scope.recargasChart, 'myChart3');
 
     };
 
